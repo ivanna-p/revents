324ddcd625b404b5b188e3562248b3788b6d9a26
Ivana Petrovic <ivanapetrovic2391@gmail.com>
2021-11-05 22:13:49 +0100

    Styling homePage and adding elements and history prop to it

--- my_react/src/app/layout/App.jsx
+++ my_react/src/app/layout/App.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboards";
 import NavigationBar from "../../features/navigation/NavigationBar";
-import HomePage from '../../features/home/HomePage';
-import  EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
 import EventForm from "../../features/events/eventForm/EventForm";
 import { Route } from "react-router";
 
@@ -23,13 +23,27 @@ function App() {
 
     return (
         <>
-            <NavigationBar setFormOpen={handleCreateFormOpen} />
-            <Container className='main'>
-                    <Route exact path='/' component={HomePage} />
-                    <Route exact path='/events' component={EventDashboard} />
-                    <Route path='/events/:id' component={EventDetailedPage} />
-                    <Route path='/createEvent' component={EventForm} />
-            </Container>
+            <Route exact path='/' component={HomePage} />
+            <Route
+                path={"/(.+)"}
+                render={() => (
+                    <>
+                        <NavigationBar setFormOpen={handleCreateFormOpen} />
+                        <Container className='main'>
+                            <Route
+                                exact
+                                path='/events'
+                                component={EventDashboard}
+                            />
+                            <Route
+                                path='/events/:id'
+                                component={EventDetailedPage}
+                            />
+                            <Route path='/createEvent' component={EventForm} />
+                        </Container>
+                    </>
+                )}
+            />
         </>
     );
 }
--- my_react/src/features/home/homePage.jsx
+++ my_react/src/features/home/homePage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+    Segment,
+    Container,
+    Header,
+    Image,
+    Button,
+    Icon,
+} from "semantic-ui-react";
 
-export default function HomePage() {
+export default function HomePage({ history }) {
     return (
-        <h1>Home page</h1>
-    )
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container>
+                <Header as='h1' inverted>
+                    <Image
+                        size='massive'
+                        src='/assets/logo.png'
+                        style={{ magrinBottom: 12 }}
+                    />
+                    Re-vents
+                </Header>
+                <Button
+                    onClick={() => history.push("/events")}
+                    size='huge'
+                    inverted
+                >
+                    Get started
+                    <Icon name='right arrow' inverted />
+                </Button>
+            </Container>
+        </Segment>
+    );
 }
