d8a5b1f58d736cbf41ecb97cb3c2d33bd5238b5f
Ivana Petrovic <ivanapetrovic2391@gmail.com>
2021-11-09 13:26:59 +0100

    Configuring Store and creating test file Sandbox

--- my_react/src/features/sandbox/Sandbox.jsx
+++ my_react/src/features/sandbox/Sandbox.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { decrement, increment } from "./testReducer";
 
-export default function Sandbox () {
-
-const data = useSelector(state => state.data);
+export default function Sandbox() {
+    const dispatch = useDispatch();
+    const data = useSelector((state) => state.data);
 
     return (
         <>
             <h1>Testing 224</h1>
             <h3>The data is: {data}</h3>
-        </> 
-    )
+            <Button
+                onClick={() => dispatch(decrement(5))}
+                content='decrement'
+                color='red'
+            />
+            <Button
+                onClick={() => dispatch(increment(10))}
+                content='increment'
+                color='green'
+            />
+        </>
+    );
 }
