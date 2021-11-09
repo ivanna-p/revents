3d27d29be571e446db709102b92d8021e3820c75
Ivana Petrovic <ivanapetrovic2391@gmail.com>
2021-11-09 21:30:35 +0100

    Adding actions and dispatching them

--- my_react/src/features/events/eventDashboard/EventListItem.jsx
+++ my_react/src/features/events/eventDashboard/EventListItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Item, Segment, Icon, List, Button} from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { deleteEvent } from "../eventActions";
 
-export default function EventListItem({ event, selectEvent, deleteEvent }) {
+export default function EventListItem({ event }) {
+    const dispatch = useDispatch();
+
     return (
         <Segment.Group>
             <Segment>
@@ -44,7 +48,7 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
             <Segment clearing>
                 <div>{event.description}</div>
                 <Button
-                    onClick={() => deleteEvent(event.id)}
+                    onClick={() => dispatch(deleteEvent(event.id))}
                     color='red'
                     floated='right'
                     content='Delete'
